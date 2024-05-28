05f0f95648f18e6b30b65f97ceeea3dd64aaa0c7
paizanadrian <paizanadrian@gmail.com>
2024-05-29 01:46:23 +0300

    Add button

--- web.py
+++ web.py
@@ -3,28 +3,28 @@ import functions
 
 todos = functions.get_todos()
 
-
 def add_todo():
     todo_local = st.session_state["new_todo"]
-    # print(todo_local)
     todos.append(todo_local)
     functions.write_todos(todos)
     st.session_state["new_todo"] = ""
 
+def delete_todo(index):
+    todos.pop(index)
+    functions.write_todos(todos)
+    st.rerun()
 
 st.title("My Todo App")
 st.subheader("This is my todo app!")
 st.write("This app is to increase your productivity.")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
-    if checkbox:
-        # print(checkbox)
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[todo]
-        st.rerun()
-
+    col1, col2 = st.columns([0.8, 0.2])
+    with col1:
+        st.text(todo)
+    with col2:
+        if st.button("Delete", key=f"delete_{index}"):
+            delete_todo(index)
 
 st.text_input(label="Adauga ToDo", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
