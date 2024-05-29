258e4abec5d702537a2b8f104f51215c39130a50
paizanadrian <paizanadrian@gmail.com>
2024-05-29 21:59:34 +0300

    Add index

--- web.py
+++ web.py
@@ -14,31 +14,8 @@ def add_todo():
 def delete_todo(index_local):
     todos.pop(index_local)
     functions.write_todos(todos)
-    st.rerun()
-# Adaugă stiluri CSS
+    # st.rerun()
 
-
-st.markdown("""
-    <style>
-    .delete-button {
-        background-color: red;
-        color: white;
-        border: none;
-        padding: 0.5em 1em;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        
-        font-size: 1em;
-        margin: 0.2em;
-        cursor: pointer;
-        border-radius: 4px;
-    }
-    .delete-button:hover {
-        background-color: darkred;
-    }
-    </style>
-    """, unsafe_allow_html=True)
 
 st.title("My Todo App")
 st.subheader("This is my todo app!")
@@ -47,15 +24,13 @@ st.write("This app is to increase your productivity.")
 for index, todo in enumerate(todos):
     col1, col2 = st.columns([0.8, 0.2])
     with col1:
-        st.text(todo)
+        st.text(f"{index + 1}. " + todo)
     with col2:
-        # Folosește stilul CSS personalizat pentru butonul de ștergere
-        if st.button("Delete", key=f"delete_{index}", on_click=lambda i=index: delete_todo(i),
-                     args=(index,), help="Click to delete this todo", use_container_width=True):
-            st.markdown(f'<button class="delete-button">Delete</button>', unsafe_allow_html=True)
+        if st.button("Delete", type="primary", key=f"delete_{index}", on_click=lambda i=index: delete_todo(i),
+                     args=(index,), use_container_width=True):
+            pass
 
 st.text_input(label="Adauga ToDo", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
 
-print("Is Working!")
-# streamlit run web.py
+print("It's Working!")
